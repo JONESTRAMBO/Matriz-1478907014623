866ff94ef41119aacd922f55970792dbbb643bf8
Desarrollo <sedpc.sa@hotmail.com>
2016-11-12 12:34:44 -0700

    Prueba

---
+++ src/dotnetstarter/wwwroot/Character.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnethelloworld.wwwroot
+{
+    public class Character
+    {
+        public string Name;
+    }
+}
